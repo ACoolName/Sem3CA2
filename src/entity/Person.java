c8c0aa6f749e6cf7562d71ae83631369ecd974d5
TeodorDumitru <TeoDum@gmail.com>
2014-09-29 15:31:10 +0200

    Added test for Person entity class.
    Extreme Programming: Martin Gergov, Teodor Dumitru

--- src/entity/Person.java
+++ src/entity/Person.java
@@ -7,9 +7,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 
 @Entity
+@NamedQueries({
+    @NamedQuery(name = "Person.findAll", query = "SELECT p FROM Person p ORDER BY p.firstName")
+})
 public class Person implements Serializable {
 
     private static final long serialVersionUID = 1L;
