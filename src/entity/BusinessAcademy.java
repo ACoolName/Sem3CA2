b6b88dc5e0fca6c926e3d789ae978d1b24f2b434
Unskipp <>
2014-10-02 12:37:18 +0200

    Fixed the tables error.
    Programmer : Raul

--- src/entity/BusinessAcademy.java
+++ src/entity/BusinessAcademy.java
@@ -3,37 +3,51 @@ package entity;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.OneToMany;
 
 @Entity
 public class BusinessAcademy implements Serializable {
+
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    
+
     @OneToMany
+    @JoinTable(name = "BUS_PERSON",
+            joinColumns = {
+                @JoinColumn(name = "PERSON_ID", referencedColumnName = "ID")},
+            inverseJoinColumns = {
+                @JoinColumn(name = "BUSINESSACADEMY_ID", referencedColumnName = "ID")})
     private List<Person> staffStudents = new ArrayList();
-    
+
     @OneToMany
+    @JoinTable(name = "BUS_COURSE",
+            joinColumns = {
+                @JoinColumn(name = "COURSE_ID", referencedColumnName = "ID")},
+            inverseJoinColumns = {
+                @JoinColumn(name = "BUSINESSACADEMY_ID", referencedColumnName = "ID")})
     private List<Course> courses = new ArrayList();
-    
+
     @OneToMany
     private List<ClassRoom> classes = new ArrayList();
-    
+
     private String name;
 
     public BusinessAcademy(String name) {
         this.name = name;
     }
-    
+
     public BusinessAcademy() {
     }
-    
+
     public String getName() {
         return name;
     }
@@ -50,5 +64,5 @@ public class BusinessAcademy implements Serializable {
     public String toString() {
         return "entity.BusinessAcademy[ id=" + id + " ]";
     }
-    
+
 }
